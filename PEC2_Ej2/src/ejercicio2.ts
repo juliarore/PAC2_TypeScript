7ff410188f3073402f09f5c764302d6867652c97
Júlia Romero <juliarore6@gmail.com>
2025-11-01 16:48:15 +0100

    feat: rename ejercicio2.txt to ejercicio2.ts and implement HangarHash with print loop

---
+++ PEC2_Ej2/src/ejercicio2.ts
@@ -0,0 +1,30 @@
+// Sustituye /***/ por las instrucciones adecuadas que cumplan las operaciones y salidas indicadas en los comentarios.
+
+
+interface Plane {
+    model:string,
+    npassengers:number
+}
+
+type HangarHash = Record<string, Plane>;
+
+let myHangar:HangarHash = {}
+
+myHangar['123Z'] = {
+    model:'airbus',
+    npassengers:200
+}
+myHangar['H789'] = { 
+    model:'boeing',
+    npassengers:151
+}
+
+/** Print following lines (going through the object)
+ * 123Z:airbus(200)
+ * H789:boeing(151)
+ */
+
+for (const id in myHangar) {
+    const plane = myHangar[id];
+    console.log(`${id}:${plane.model}(${plane.npassengers})`);
+}
