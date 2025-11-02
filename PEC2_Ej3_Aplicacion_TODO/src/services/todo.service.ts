1eb10013b955c559a6652fb0f4db34ae5c627e1d
Júlia Romero <juliarore6@gmail.com>
2025-11-02 17:35:09 +0100

    feat: migrate Todo service from JS to TypeScript

--- PEC2_Ej3_Aplicacion_TODO/src/services/todo.service.ts
+++ PEC2_Ej3_Aplicacion_TODO/src/services/todo.service.ts
@@ -1,50 +1,56 @@
+import { Todo } from "../models/todo.model.js";
+
 /**
  * @class Service
  *
  * Manages the data of the application.
  */
-class TodoService {
+export class TodoService {
+  public todos: Todo[];
+  private onTodoListChanged: (todos: Todo[]) => void;
+
   constructor() {
-    this.todos = (JSON.parse(localStorage.getItem("todos")) || []).map(
-      todo => new Todo(todo)
-    );
+    const storedTodos = localStorage.getItem("todos");
+    const parsedTodos = storedTodos ? JSON.parse(storedTodos) : [];
+    this.todos = parsedTodos.map((todo: Todo) => new Todo(todo));
+    this.onTodoListChanged = () => {}; 
   }
 
-  bindTodoListChanged(callback) {
+  bindTodoListChanged(callback: (todos: Todo[]) => void): void {
     this.onTodoListChanged = callback;
   }
 
-  _commit(todos) {
+  private _commit(todos: Todo[]): void {
     this.onTodoListChanged(todos);
     localStorage.setItem("todos", JSON.stringify(todos));
   }
 
-  addTodo(text) {
+  addTodo(text: string): void {
     this.todos.push(new Todo({ text }));
 
     this._commit(this.todos);
   }
 
-  editTodo(id, updatedText) {
+  editTodo(id: string, updatedText: string): void {
     this.todos = this.todos.map(todo =>
       todo.id === id
         ? new Todo({
-            ...todo,
-            text: updatedText
-          })
+          ...todo,
+          text: updatedText
+        })
         : todo
     );
 
     this._commit(this.todos);
   }
 
-  deleteTodo(_id) {
+  deleteTodo(_id: string): void {
     this.todos = this.todos.filter(({ id }) => id !== _id);
 
     this._commit(this.todos);
   }
 
-  toggleTodo(_id) {
+  toggleTodo(_id: string): void {
     this.todos = this.todos.map(todo =>
       todo.id === _id ? new Todo({ ...todo, complete: !todo.complete }) : todo
     );
