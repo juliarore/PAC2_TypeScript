6b08841b4840a4ed4f7fdf893b1135e40cb54dbc
Júlia Romero <juliarore6@gmail.com>
2025-11-02 12:17:27 +0100

    feat: migrate Todo model from JS to TypeScript

---
+++ PEC2_Ej3_Aplicacion_TODO/src/models/todo.model.ts
@@ -0,0 +1,30 @@
+/**
+ * @class Model
+ *
+ * Manages the data of the application.
+ */
+
+export class Todo {
+
+  // Definim les propietats del model i el seu tipat
+  id: string;
+  text: string;
+  complete: boolean;
+
+  // Afegim tipat als paràmetres del constructor
+  constructor({ text, complete = false }: { text: string; complete?: boolean }) {
+    this.id = this.uuidv4();
+    this.text = text;
+    this.complete = complete;
+  }
+
+  private uuidv4(): string {
+    // Afegim ('' + [1e7] + ...) perquè TypeScript entengui que és una cadena, no un array
+    return ('' + [1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+      (
+        Number(c) ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (Number(c) / 4)))
+      ).toString(16)
+    );
+  }
+}
